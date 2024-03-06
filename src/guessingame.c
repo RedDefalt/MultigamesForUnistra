fa08efd134e22972e54da7c9af34ce6058601b8a
Hrayr Mirzoyan <hrayrmirzoyan@etu.unistra.fr>
2024-03-06 15:04:59 +0100

    ajout de la fonction principale guessingame qui initialise le jeu et assemble les fonctions

--- src/guessingame.c
+++ src/guessingame.c
@@ -26,3 +26,21 @@ void instruction(int user_input, int value_to_guess) {
         printf("Essayer avec une valeur plus grande\n");
     }
 }
+
+int guessingame() {
+    structureGuessinGame* jeu = init_game();
+    int still_play = 1;
+    int value_to_guess = alea();
+    do {
+        int value = user_input();
+        add_elem(jeu, value);
+        if (continues(value, value_to_guess)) {
+            instruction(value, value_to_guess);
+        } else {
+            still_play = 0;
+        }
+    } while (still_play);
+    printf("Felicitation vous avez trouver le nombre au bout de %d esseai \n", taille(jeu) +1 );
+    free(jeu);
+    return 0;
+}
