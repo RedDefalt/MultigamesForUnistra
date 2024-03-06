59001e2aee5dd4fda0556ab3024dee1b25a4e8e5
Hrayr Mirzoyan <hrayrmirzoyan@etu.unistra.fr>
2024-03-06 13:48:10 +0100

    réparation et supression des primitives des fonctions

--- inc/structureGuessinGame.h
+++ inc/structureGuessinGame.h
@@ -5,10 +5,3 @@ typedef struct guessingame {
     int nbr_elem;
 } structureGuessinGame;
 
-guessingame* init_game();
-
-guessingame* nbr_de_essai(guessingame* game);
-
-guessingame* add_elem(guessingame* game, int nbr);
-
-guessingame* existe_deja(guessingame* game, int nbr);
