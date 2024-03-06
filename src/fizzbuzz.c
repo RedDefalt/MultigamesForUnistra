c9514717bb765699f10d621bb5864e79be84b1f5
Hrayr Mirzoyan <hrayrmirzoyan@etu.unistra.fr>
2024-03-06 16:02:25 +0100

    ajout de la fonction condition_controll qui return FIZZ si le nbr est divisible par 3, BUZZ si par 5, FIZZBUZZ si par 3 et 5 et le nbr si par aucun des 2

--- src/fizzbuzz.c
+++ src/fizzbuzz.c
@@ -8,3 +8,15 @@ int user_input() {
     }
     return valeur;
 }
+
+void condition_controll(int user_input) {
+    if (user_input % 3 == 0 && user_input % 5 == 0) {
+        printf("FIZZBUZZ\n");
+    } else if (user_input % 5 == 0) {
+        printf("BUZZ\n");
+    } else if (user_input % 3 == 0) {
+        printf("FIZZ\n");
+    } else {
+        printf("%d\n",user_input);
+    }
+}
